f164bf2a5033729fb94cb63e24477732d9dbd692
P57749 <polopepe567@gmial.com>
2023-09-14 23:35:47 -0400

    0.1.1

--- server/script.js
+++ server/script.js
@@ -1,17 +1,19 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.getElementById('chat-messages');
 
-chatForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const messagesInput = document.getElementById('message');
-    const message = messagesInput.value.trim();
+chatForm.addEventListener('submit', function(event) {
+  event.preventDefault();
 
-    if (message) {
-        const messageElement = document.getElementById('div');
-        messageElement.textContent = message;
-        chatMessages.appendChild(messageElement);
-        messageElement.value = '';
-    }
+  const message = chatForm.elements['message'].value;
+  chatForm.elements['message'].value = '';
+
+  const newMessage = document.createElement('p');
+  newMessage.textContent = message;
+
+  // Add classes based on message type
+  newMessage.classList.add('message');
+  newMessage.classList.add('message-outgoing'); // Assuming it's an outgoing message
 
 
+  chatMessages.appendChild(newMessage);
 });
